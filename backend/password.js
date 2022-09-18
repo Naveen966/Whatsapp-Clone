9b858d39fdef4b9f620302f07e7739f34ce08b29
naveen966 <naveenkumar85600@gmail.com>
2022-09-18 16:25:00 +0530

    commit second

--- backend/password.js
+++ backend/password.js
@@ -1,3 +1,5 @@
 const mongoDBMainURL = `mongodb+srv://registration:wvRVIXJ7liv89err@userdata.kxhufrl.mongodb.net/?retryWrites=true&w=majority`;
 
 module.exports = mongoDBMainURL;
+
+// hi i am trying to check that does git save updates or not.
